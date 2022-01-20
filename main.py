4a0a041c5bb9ea77d80e44c3e9a2f94631677f2d
DragonAss-DEV <71887969+DragonASS-Dev@users.noreply.github.com>
2022-01-20 01:06:28 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+#develop by DragonASS-Dev
+# my Github https://github.com/DragonASS-Dev/
+# Version: 2.3 (15.1.22)
+# This code is open  source just update the log.md
+patient=(int(input('Alter eingeben\n-> ')))
+age=(str(patient/2+7))
+old=(str(patient*1.6-7))
+if patient>400:
+  comand=input('--------Easteregg Menu--------\neasteregg --> ')
+  if comand=='hentai':
+    print('Visit https://hanime.tv NOW')
+  elif comand==('loli'):
+    print('⠃⣸⡀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⣆⢹⣿⣿⣿⡈⢿⣿⣿⣿⣿\n⠄⢻⡇⠄⢛⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⡆⠹⣿⣆⠸⣆⠙⠛⠛⠃⠘⣿⣿⣿⣿\n⠄⠸⣡⠄⡈⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⣠⣉⣤⣴⣿⣿⠿⠿⠿⡇⢸⣿⣿⣿\n⠄⡄⢿⣆⠰⡘⢿⣿⠿⢛⣉⣥⣴⣶⣿⣿⣿⣿⣻⠟⣉⣤⣶⣶⣾⣿⡄⣿⡿⢸\n⠄⢰⠸⣿⠄⢳⣠⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⡇⢻⡇⢸\n⢷⡈⢣⣡⣶⠿⠟⠛⠓⣚⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⠇⠘\n⡀⣌⠄⠻⣧⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⢿⣿⣿⣿⣿⣿⡟⠘⠄⠄\n⣷⡘⣷⡀⠘⣿⣿⣿⣿⣿⣿⣿⣿⡋⢀⣠⣤⣶⣶⣾⡆⣿⣿⣿⠟⠁⠄⠄⠄⠄\n⣿⣷⡘⣿⡀⢻⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣿⣿⣿⣿⣷⡿⠟⠉⠄⠄⠄⠄⡄⢀\n⣿⣿⣷⡈⢷⡀⠙⠛⠻⠿⠿⠿⠿⠿⠷⠾⠿⠟⣛⣋⣥⣶⣄⠄⢀⣄⠹⣦⢹⣿')
+  elif comand=='test':
+    print('Version: 2.3\ndeveloper: DragonASS-Dev\nGitHub: https://github.com/DragonASS-Dev/')
+  elif comand=='anime':
+    print('⣿⡇⣿⣿⣿⠛⠁⣴⣿⡿⠿⠧⠹⠿⠘⣿⣿⣿⡇⢸⡻⣿⣿⣿⣿⣿⣿⣿\n⢹⡇⣿⣿⣿⠄⣞⣯⣷⣾⣿⣿⣧⡹⡆⡀⠉⢹⡌⠐⢿⣿⣿⣿⡞⣿⣿⣿\n⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿\n⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿\n⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮\n⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀\n⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸\n⠄⠄⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀\n⠄⠄⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾\n⠄⠄⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿\n⠒⠤⠄⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇⠄⢸⣿⣿⣿')
+  elif comand=='horny':
+    print('Oi Oi Oi! Do u have a loicense for that?! Bein Horny, in the middle of town?!\n⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣔⢧⣓⢖⡮⣎⢮⢣⡫⡂⠄⠄.⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⠄⠄⠄⠄⠄⢠⡺⣧⣳⢵⡳⡝⡎⣎⢇⢏⢎⢲⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡠⡄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⠄⠄⠄⠄⡀⡧⣫⡳⡳⡓⡝⢚⢻⢽⢺⢼⢼⢼⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠠⣀⢀⠄⠄⣠⢪⢣⠊⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⠄⢀⢠⡪⣞⣝⣞⢮⡳⡱⢌⢂⠢⡑⡸⡸⡹⣝⡾⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⡱⡀⠈⠄⡀⡮⡪⠊⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⠄⢠⢞⣜⢮⢮⢺⢜⢷⢽⢮⢇⢇⢕⢵⡧⣇⢇⢗⡿⡻⠄⠄⠄⠄⠄⠄⢠⠄⢄⠈⠂⠄⢀⢖⢝⠜⠄⠄⡀⠄⠄⠄⠄⠄⠄⠄⠄\n⠄⠄⡠⢯⢯⡺⡧⣳⡱⢍⠇⡏⢎⠫⡢⢊⠜⢜⢕⠝⠁⠄⠄⠄⠄⠄⠄⣀⢄⠈⠢⠜⠄⢀⢔⢇⡯⣢⢆⡶⡤⣄⡀⡐⠄⠁⠄⠐⠄⠄\n⠄⢠⣝⢵⡫⡿⣝⢮⡪⡣⡓⠜⢔⢑⠌⢔⠨⢂⠢⠡⠄⠄⠄⠄⠄⠄⠄⠙⢆⠅⠄⠄⡆⡧⣫⡺⡼⡪⢷⢽⢯⣗⢯⡆⡄⠄⠄⠄⠄⠄\n⠄⣞⢾⢵⡫⡯⣗⣝⢮⢢⠪⡘⡐⢅⢊⠔⡨⠢⡑⡅⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢮⣺⢽⢳⡹⡰⡨⠪⡽⣕⢯⡳⣝⡦⡀⠄⠄⡀⠄\n⠄⣞⢽⣝⢮⢯⣺⢮⣳⡳⡵⡸⡨⡢⣑⢑⢌⢎⠪⡢⠄⠄⠄⠄⠄⠄⠄⠄⢠⠪⣳⣟⢮⢳⢕⣽⢪⢘⢜⢜⢮⡳⡽⣕⡯⣖⠄⠄⠄⠄\n⠄⠘⣝⢮⢯⣳⣳⣻⣺⡪⣳⢱⢱⢸⢰⢵⡕⡔⡑⡌⡀⠄⠄⠄⠄⠄⢀⠦⠃⠁⢻⣽⢕⣗⡽⡳⡱⡐⢕⢕⡳⡽⣝⡮⣯⡺⣕⡀⠄⠄\n⠄⠄⠈⠹⢳⣳⣻⣞⡧⣏⢮⢪⠪⡪⡪⡳⡽⣰⢨⢊⠪⠪⠢⡢⢄⢔⠍⠄⠄⠄⠈⠈⢑⢗⠝⢜⠰⡘⣜⢼⣝⣞⣗⢯⡳⣝⢮⢆⠄⠄\n⠄⠄⠄⠄⠄⠈⠘⠷⢟⢮⡳⡱⣑⢌⠪⠨⠪⠺⠸⡨⡢⢄⣀⢰⠍⠄⠄⠄⠄⠄⠄⡀⢘⢌⢎⠪⡪⡪⡮⣗⢗⣗⢽⢕⢯⢮⡳⣯⠄⠄\n⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⡮⡣⡁⠄⠄⠄⠄⠄⠄⠈⠨⡢⡪⠢⠋⠄⠄⠄⠄⠄⠁⠄⠨⡢⡑⡅⢕⠕⣕⢳⢹⢸⢪⢳⡹⡮⡯⡷⡅⠄\n⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢯⢎⢆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠁⠄⠄⠄⠄⠈⠄⠄⠄⠈⠂⠑⠘⠐⠑⠘⠘⠘⠘⠘⠊⠙⠙⠙⠋⠃⠄')
+  elif comand=='login':
+    input('Log In Menu\nUsername: ')
+    input('Password:')
+    print('you are stupid')
+  elif comand=='schinken':
+    print('SCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN\nSCHINKEN')
+  elif comand=='jo':
+    print('jo')
+  elif comand=='developer':
+    print('you are not the developer')
+  elif comand=='whoami':
+    print('you are a user ')
+  elif comand==('nur die spitze'):
+    print('Downloading at hanime.tv/shojou-kyouiku-re-2\nDownload 2%\nDownload 5%\nDownload 15%\nDownload 59%\nDownload 90%\nDownload 99%\nDownload sucsessfull\n\nMedia downloaded in User/Files/Sharedfiles/Admin')
+  elif comand==('no idea'):
+    print('empy')
+  elif comand==('onlyfans'):
+    print('https://bit.ly/3A6IjWg')
+  elif comand == ("deine mutter"):
+    print ("schwitz beim kacken\nund klaut bei kik\ndeine mutter\ndeine mutter")
+  elif comand==('neko'):
+    print('⣿⡟⠙⠛⠋⠩⠭⣉⡛⢛⠫⠭⠄⠒⠄⠄⠄⠈⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⡇⠄⠄⠄⠄⣠⠖⠋⣀⡤⠄⠒⠄⠄⠄⠄⠄⠄⠄⠄⠄⣈⡭⠭⠄⠄⠄⠉⠙\n⣿⡇⠄⠄⢀⣞⣡⠴⠚⠁⠄⠄⢀⠠⠄⠄⠄⠄⠄⠄⠄⠉⠄⠄⠄⠄⠄⠄⠄⠄\n⣿⡇⠄⡴⠁⡜⣵⢗⢀⠄⢠⡔⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n⣿⡇⡜⠄⡜⠄⠄⠄⠉⣠⠋⠠⠄⢀⡄⠄⠄⣠⣆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸\n⣿⠸⠄⡼⠄⠄⠄⠄⢰⠁⠄⠄⠄⠈⣀⣠⣬⣭⣛⠄⠁⠄⡄⠄⠄⠄⠄⠄⢀⣿\n⣏⠄⢀⠁⠄⠄⠄⠄⠇⢀⣠⣴⣶⣿⣿⣿⣿⣿⣿⡇⠄⠄⡇⠄⠄⠄⠄⢀⣾⣿\n⣿⣸⠈⠄⠄⠰⠾⠴⢾⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⢁⣾⢀⠁⠄⠄⠄⢠⢸⣿⣿\n⣿⣿⣆⠄⠆⠄⣦⣶⣦⣌⣿⣿⣿⣿⣷⣋⣀⣈⠙⠛⡛⠌⠄⠄⠄⠄⢸⢸⣿⣿\n⣿⣿⣿⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠈⠄⠄⠄⠄⠄⠈⢸⣿⣿\n⣿⣿⣿⠄⠄⠄⠘⣿⣿⣿⡆⢀⣈⣉⢉⣿⣿⣯⣄⡄⠄⠄⠄⠄⠄⠄⠄⠈⣿⣿\n⣿⣿⡟⡜⠄⠄⠄⠄⠙⠿⣿⣧⣽⣍⣾⣿⠿⠛⠁⠄⠄⠄⠄⠄⠄⠄⠄⠃⢿⣿\n⣿⡿⠰⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠩⠔⠒⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠘⣿\n⣿⠃⠃⠄⠄⠄⠄⠄⠄⣀⢀⠄⠄⡀⡀⢀⣤⣴⣤⣤⣀⣀⠄⠄⠄⠄⠄⠄⠁⢹')
+  elif comand =='windows':
+    print('Linux is better')
+  elif comand =='macos':
+    print('do you mean pay for slow linux ')
+  elif comand=='linux':
+    print('Good choice')
+  elif comand =='420':
+    print('░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n░░░░░░░░░.▄██░░░░░░░░░░░░░░░░░░░░░\n░░░░░░░░.▄███░░▄███▄░░░▄▄▄▄░░░░░░░\n░░░░░░░.██▀██░░██▀██░░██▀▀██░░░░░░\n░░░░░.▄██▄▄██▄░░▄██▀░░██░░██░░░░░░\n░░░░░░░░░░.██░░███▄▄░░██▄▄██░░░░░░\n░░░░░░░░░.▄██▄░░░░░░░░░▀▀▀▀░░░░░░░\n░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n')
+elif patient<13:
+  print('Was machst du mit der Anwendung geh lieber im Sandkasten Splielen')
+elif patient>40:
+  print('du bist zu alt geh ins heim zurück und belästige die altenpflegerin')
+else:
+  print('Du darfst mit',age,'Jährigen und maximal mit',old,'Jährigen ficken')
+input('\nDrück Enter um zu beenden: ')
